f007728a18d78b28d6109c237db3953d5be088ba
Andrew <smithme2000@gmail.com>
2019-05-23 12:17:22 -0400

    basic 24 works

--- www/js/game.js
+++ www/js/game.js
@@ -6,6 +6,7 @@ Math.__proto__.inclusiveRandom=function(min,max){
     max++; 
     return Math.floor(Math.random() * (+max - +min)) + +min; 
 }
+//TODO permutations
 Math.__proto__.getCombinations=function(array, size, start, initialStuff, output) {
     if (initialStuff.length >= size) {
         output.push(initialStuff);
@@ -39,12 +40,10 @@ Math.__proto__.getAllPossibleCombinations=function(array) {
                     }
                 }
                 holder=temp;
-                //holder=holder.substring(0,holder.indexOf(array[k]+""))+holder.substring(holder.indexOf(array[k]+"")+(""+array[k]).length);
             }
         }
         if(hasAll){
             returnArr.push(output[i]);
-            //console.log(output[i]);
         }
         var uniques=[returnArr[0]];
         for(var a=0;a<returnArr.length;a++){
@@ -54,7 +53,6 @@ Math.__proto__.getAllPossibleCombinations=function(array) {
                     alreadyCounted=true;
                 }
             }
-           //console.log(returnArr[a],uniques,alreadyCounted)
             if(!alreadyCounted){
                 uniques.push(returnArr[a]);
             }
@@ -77,16 +75,20 @@ var arrayMath={
     }
 }
 var sm={
-    add:function(a,b){
+    add:function(a,b,sp){
         return a+b;
     },
-    sub:function(a,b){
-        return a-b;
+    sub:function(a,b,sp){
+        if(sp){
+        return Math.abs(a-b);
+        }else{
+            return a-b;
+        }
     },
-    mult:function(a,b){
+    mult:function(a,b,sp){
         return a*b;
     },
-    div:function(a,b){
+    div:function(a,b,sp){
         return a/b;
     },
 }
@@ -96,7 +98,7 @@ var game={
     /**using: the number of numbers being played withh */
     using:4,
     /**the smallest starting number */
-    minium:0,
+    minium:1,
     /**the largest starting number */
     maxium:13,
     /**an object of legal math operators */
@@ -116,34 +118,84 @@ var game={
             game.set.push(Math.inclusiveRandom(game.minium,game.maxium));
         }
     },
-
     displayGame:function(){
+        var elem=$(pageElements.cardsHolder);
         for(var i=0;i<game.set.length;i++){
-            $(pageElements.cardsHolder).append("<div class='card' onclick='game.selectElement(this)'><div class='number' id="+i+">"+game.set[i]+"</div></div>");
+            elem.append("<div class='card not-selected' data-numbIndex="+i+"><div class='number' id="+i+">"+game.set[i]+"</div></div>");
         }
+         $(".card").click(function(){game.selectElement(this)});
+         elem.append("<div id='ops' class='main-container'></div>");
+         var ops=$("#ops");
+         var opKeys=Object.keys(game.operators);
+         var mathKey=Object.keys(sm);
+         for(var j=0,k=0;j<opKeys.length,k<mathKey.length;j++,k++){
+             ops.append("<div class='op'data-index='"+j+"'><div class='center'>"+game.operators[opKeys[j]]+"</div></div>");
+         }
+          $(".op").click(function(){game.performOperation(this)});
     },
     selectElement:function(element){
-        var id=0;
-        if(game.selectedNumbers.length>2){
-            game.selectedNumbers=[[game.selectedNumbers[0]]]
-        }else if(game.selectedNumbers.indexOf([id])>-1){
-            var holder=[]
+        console.log("selecting")
+        var id=parseInt(element.firstChild.id);
+        console.log(game.selectedNumbers,id,game.selectedNumbers.indexOf(id),game.selectedNumbers.length>1)
+        if(game.selectedNumbers.length>1){
+            game.selectedNumbers=[game.selectedNumbers[0],game.selectedNumbers[1]]
+            //console.log(game.selectedNumbers.length)
+            if(game.selectedNumbers.indexOf(id)>-1){
+                console.log("h19")
+                $("#"+id).css("border","0px solid black");
+                var holder=[];
+                for(var i=0;i<game.selectedNumbers.length;i++){
+                    if(game.selectedNumbers[i]!=id){
+                        holder.push(game.selectedNumbers[i])
+                    }
+                }
+                game.selectedNumbers=holder;
+            }
+        }else if(game.selectedNumbers.indexOf(id)>-1){
+            var holder=[];
+            $("#"+id).css("border","0px solid black")
+            console.log("h2")
             for(var i=0;i<game.selectedNumbers.length;i++){
                 if(game.selectedNumbers[i]!=id){
                     holder.push(game.selectedNumbers[i]);
+                }else{
+                   /* $("#"+game.selectedNumbers[i]).css("border","0px solid black")
+                    console.log("h3")*/
                 }
             }
             game.selectedNumbers=holder;
         }else{
+            console.log("h5")
              game.selectedNumbers.push(id);
         }
         for(var i=0;i<game.selectedNumbers.length;i++){
-            $("#"+game.selectedNumbers[i]).addClass("selelected");
+            $("#"+game.selectedNumbers[i]).removeClass("not-selected");
+            $("#"+game.selectedNumbers[i]).css("border","2px solid green");
         }
+        //console.log("fin")
     },
 
-    performOperation:function(){
-
+    performOperation:function(element){
+        $(".card").off("click");
+        if(game.selectedNumbers.length>1){
+        var i=parseInt(element.getAttribute("data-index"));
+        var keys=Object.keys(sm);
+        var selected=[];
+        var cards=$(".card");
+        for(var k=0;k<cards.length;k++){
+            if(parseInt(cards[k].getAttribute("data-numbIndex"))===game.selectedNumbers[0]||parseInt(cards[k].getAttribute("data-numbIndex"))===game.selectedNumbers[1]){
+                selected.push(parseInt(cards[k].innerText));
+                cards[k].outerHTML="";
+            }
+           // console.log(cards[k],cards[k].getAttribute("data-numbIndex"),game.selectedNumbers[0],game.selectedNumbers[1])
+        }
+        var newNumb=sm[keys[i]](selected[0],selected[1],true)
+        var j=Math.min(game.selectedNumbers[1],game.selectedNumbers[0])
+        game.selectedNumbers=[];
+        $(pageElements.cardsHolder).append("<div class='card not-selected' data-numbIndex="+j+"><div class='number' id="+j+">"+newNumb+"</div></div>");
+        $(".card").click(function(){game.selectElement(this)});
+        game.checkSolution()
+        }
     },
 
     updateDisplay:function(){
@@ -207,6 +259,21 @@ var game={
             return resault;
         }
     },
+
+    checkSolution:function(){
+        var x= parseInt($(".card")[0].innerText)==game.n&&$(".card").length>-1;
+        if(x){
+             $(pageElements.cardsHolder).append("<div id='winner'>Winner</div>")
+             $("#winner").click(function(){game.reset()})
+        }
+    },
+    reset:function(){
+        $(pageElements.cardsHolder)[0].innerHTML="";
+        game.set=[];
+        game.selectedNumbers=[];
+        game.generateGame();
+        game.displayGame();
+    }
    
 
 }
